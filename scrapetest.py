de60d4ad0fa6c82ba9601300b17518436d63de08
aleksandr-jc <aleksandr.robu@icloud.com>
2024-05-21 15:23:03 +0300

    get list of book names

--- scrapetest.py
+++ scrapetest.py
@@ -1,4 +1,15 @@
 from urllib.request import urlopen
+from bs4 import BeautifulSoup
 
-html = urlopen('http://pythonscraping.com/pages/page1.html')
-print(html.read())
+with open('/Users/oleksandrrobu/LinaProject/view-page-source.com-www.yakaboo.ua_ua_knigi_hudozhestvennaja-literatura.html?book_publication=Bumazhnaja.html') as file:
+    src = file.read()
+
+soup = BeautifulSoup(src, 'lxml')
+
+find_all_names = soup.find_all('a', class_='ui-card-title category-card__name')
+book_titles = []
+
+for name in find_all_names:
+    book_titles.append(name.get('title'))
+
+print(book_titles)
