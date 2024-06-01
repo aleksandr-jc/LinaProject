af7df8f0f3dc08a72d80143bc877b77c421475b8
aleksandr-jc <aleksandr.robu@icloud.com>
2024-06-01 12:54:28 +0300

    Підготував парсинг для 2021 року

--- data/nbuv/2021/main/2021corect_json_to_excel.py
+++ data/nbuv/2021/main/2021corect_json_to_excel.py
@@ -3,6 +3,7 @@ import pandas as pd
 """
 Заміна в json '][' на ',' та зміна формату json на excel, файл не змінюється
 """
+year_of_search = '2021'
 def fix_json_format(input_file):
     # Читаємо вміст файлу
     with open(input_file, 'r', encoding='utf-8') as file:
@@ -26,4 +27,4 @@ def json_to_excel(input_file, output_file):
     df.to_excel(output_file, index=False, engine='openpyxl')
 
 # Використання функції
-json_to_excel('data/nbuv/2022/main/nbuv_2022.json', 'data/nbuv/2022/main/nbuv_2022.xlsx')
+json_to_excel(f'data/nbuv/{year_of_search}/data/nbuv_{year_of_search}.json', f'data/nbuv/{year_of_search}/data/nbuv_{year_of_search}.xlsx')
