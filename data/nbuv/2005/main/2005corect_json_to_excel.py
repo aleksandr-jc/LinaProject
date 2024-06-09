7670f8238f599d87da82b7e688757d975804eb8d
aleksandr-jc <aleksandr.robu@icloud.com>
2024-06-09 14:23:06 +0300

    Парсинг з 2010 до 2004 року зроблено

---
+++ data/nbuv/2005/main/2005corect_json_to_excel.py
@@ -0,0 +1,30 @@
+import json
+import pandas as pd
+"""
+Заміна в json '][' на ',' та зміна формату json на excel, файл не змінюється
+"""
+year_of_search = '2005'
+def fix_json_format(input_file):
+    # Читаємо вміст файлу
+    with open(input_file, 'r', encoding='utf-8') as file:
+        content = file.read()
+    
+    # Видаляємо зайві символи та об'єднуємо всі масиви в один
+    content = content.replace('][', ',')
+    
+    # Перетворюємо текст у JSON об'єкт
+    data = json.loads(content)
+    return data
+
+def json_to_excel(input_file, output_file):
+    # Виправляємо формат JSON
+    data = fix_json_format(input_file)
+    
+    # Створюємо DataFrame з JSON данихg
+    df = pd.DataFrame(data)
+    
+    # Записуємо дані у файл Excel
+    df.to_excel(output_file, index=False, engine='openpyxl')
+
+# Використання функції
+json_to_excel(f'data/nbuv/{year_of_search}/data/nbuv_{year_of_search}.json', f'data/nbuv/{year_of_search}/data/nbuv_{year_of_search}.xlsx')
