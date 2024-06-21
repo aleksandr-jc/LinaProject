7958c7f093d7143a4e634724fa61810caee898b8
aleksandr-jc <aleksandr.robu@icloud.com>
2024-06-21 15:06:42 +0300

    Закінчив парсинг readeat.com

---
+++ data/readeat/main/replace_json.py
@@ -0,0 +1,19 @@
+"""
+Замінюємо '][' , ',' і переписуємо json файл
+"""
+
+
+def replace_in_json_file(input_file):
+    # Відкриття файлу JSON для заміни
+    with open(input_file, 'r', encoding='utf-8') as file:
+        content = file.read()
+
+    # Заміна рядків
+    new_content = content.replace('][' , ',')
+
+    # Збереження оновленого вмісту у тому ж файлі
+    with open(input_file, 'w', encoding='utf-8') as file:
+        file.write(new_content)
+
+# Використання функції
+replace_in_json_file('data/readeat/data/processed/readeat_json.json')
