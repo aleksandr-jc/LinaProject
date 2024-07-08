bf9560c66a79a59282e9a0679dd00e8382329d4f
aleksandr-jc <aleksandr.robu@icloud.com>
2024-07-08 09:35:31 +0300

    Початок парсингу художніх книг з yakaboo.ua. зробив перші 10 сторінок

---
+++ data/yakaboo/main/selenium_book_list.py
@@ -0,0 +1,86 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import Select
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+import json
+
+def get_source_html(url):
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver.maximize_window()
+    info_list = []
+    try:
+        driver.get(url=url)
+        time.sleep(3)
+        
+
+        info_button = driver.find_element(By.XPATH, '//*[@id="product"]/div[1]/div/div/div/section/div[7]/button') 
+                                                #    //*[@id="product"]/div[1]/div/div/div/section/div[7]/button
+        driver.execute_script("arguments[0].scrollIntoView(true);", info_button)
+        info_button.click()
+        time.sleep(2)
+
+        book_name = driver.find_element(By.XPATH, '//*[@id="product"]/div[1]/div/div/div/section/div[1]/div[2]/h1').text
+                                                
+        main_content = driver.find_element(By.CLASS_NAME, 'chars')
+
+        results = main_content.find_elements(By.CLASS_NAME, 'char')
+
+        for result in results:
+            info_list.append(result.text.strip())
+
+        return info_list, book_name
+
+    except Exception as _ex:
+        print(_ex)
+    finally:
+        time.sleep(1)
+        driver.close()
+        driver.quit()
+
+
+
+def save_book_info(info_list):
+    # Створюємо словник для зберігання інформації
+    book_info = {}
+    
+    # Обробляємо кожен елемент списку
+    for item in info_list[0]:
+        # Розділяємо ключ і значення за символом нової лінії
+        key, value = item.split('\n')
+        
+        try:# Перетворюємо числові значення в відповідний формат
+            if key in ["Тираж", "Кількість сторінок", "Рік видання", "Вага"]:
+                value = int(value)
+        except:
+            value = value
+        
+        # Додаємо пару ключ-значення в словник
+        book_info['Назва книжки'] = info_list[1]
+        book_info[key] = value
+        
+    
+    # Зберігаємо словник у файл у форматі JSON
+    with open('book_info.json', 'a', encoding='utf-8') as json_file:
+        json.dump(book_info, json_file, ensure_ascii=False, indent=4)
+    
+    return book_info
+
+
+with open('data/yakaboo/main/book_links.txt') as file:
+    src = file.readlines()
+
+# 20 книжок
+for link in src[20:]:
+
+    strat_time = time.time()
+
+    print(f'Page: {link}')
+    link = link.strip()
+    info = get_source_html(link)
+    save_book_info(info)
+
+    end_time = time.time() - strat_time
+    print(end_time)
+    
